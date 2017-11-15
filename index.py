f8ddae6db3785c104675cc1ee5ffb09be4f38c6f
zcjtshy <see.yim.13@ucl.ac.uk>
2017-11-15 19:01:41 +0000

    index

--- index.py
+++ index.py
@@ -23,12 +23,53 @@ def top_1_restaurant(image_urls=0):
 		url = img['images']['standard_resolution']['url']
 		image_urls.append(url)
 	#','.join(image_urls) 
-	return render_template("index.html", image_urls=image_urls)
+	weather1 = get_weather()
+	return render_template("index3.html", image_urls=image_urls, weather_description=weather1)
 
 	#print pic_url= data['data'][0]['images']['standard_resolution']['url']
 	#image = data['data'][0]['images']['standard_resolution']['url']
 	#return data['images']['standard_resolution']['url']
 
+def get_weather():
+	endpoint= "http://api.openweathermap.org/data/2.5/weather"
+	payload = {"q": London,UK, "units":"metric", "appid":"e93fd407cde06db0608ffb006ea09c8a"}
+
+	response = requests.get(endpoint, params=payload)
+	data = response.json()
+
+	temperature = data["main"]["temp"]
+	name = data["name"]
+	weather = data["weather"][0]["main"]
+
+    if temperature <=5:
+    	message = 'warm'
+    else:
+        message = 'to eat'         
+	    
+	return "It's {}C in {} today, grab something {}".format(temperature, name, message)
+	
+def search_food():
+    resp = requests.get(
+                         "https://api.foursquare.com/v2/venues/explore",
+                        params = dict(
+                         client_id= "ND0RYQES1ZX3UHVOYMOP2SRUEEB1IJ4DGMURFGPWBGWY0VHT",
+                            client_secret= "TSBJQHVJS1FERM1K3QOYHBJKELC32N0IOQAUPDGLJ5TAINC4",
+                         ll= "51.519587,-0.127012",
+                        v= "20170801",
+                        section= "food"))
+
+    data = json.loads(resp.text)
+    items=data['response']['groups'][0]['items']
+  
+    restaurants_info = []
+    for item in items:
+        restaurant_info = {
+            'name': item['venue']['name'],
+            'rating': item['venue']['rating'],
+            'image': get_photos(item ['venue']['id']),
+        }
+        restaurants_info.append(restaurant_info)
+    return restaurant_info
 	
 @app.route('/example_data/')
 def example_data():
